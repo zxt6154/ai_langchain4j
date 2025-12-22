8126262ae819602ca0ecaccf9d7773af8fea3c17
zxt6154 <zxt61542990@yeah.net>
2025-12-22 18:44:22 +0800

    add

--- src/test/java/com/alice/ai_langchain4j/LLMTest.java
+++ src/test/java/com/alice/ai_langchain4j/LLMTest.java
@@ -1,5 +1,6 @@
 package com.alice.ai_langchain4j;
 
+import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class LLMTest {
     @Autowired
     private OpenAiChatModel openAiChatModel;
+    @Autowired
+    private OllamaChatModel ollamaChatModel;
 
     @Test
     public void testSpringBoot() {
@@ -22,19 +25,22 @@ public class LLMTest {
      */
     @Test
     public void testGPTDemo() {
-//初始化模型
-
-//LangChain4j提供的代理服务器，该代理服务器会将演示密钥替换成真实密钥， 再将请求转发给OpenAI API
-//.baseUrl("http://langchain4j.dev/demo/openai/v1") //设置模型api地址（如果apiKey="demo"，则可省略baseUrl的配置）
+   //LangChain4j提供的代理服务器，该代理服务器会将演示密钥替换成真实密钥， 再将请求转发给OpenAI API
+        //.baseUrl("http://langchain4j.dev/demo/openai/v1") //设置模型api地址（如果apiKey="demo"，则可省略baseUrl的配置）
         OpenAiChatModel model = OpenAiChatModel.builder()
         .apiKey("demo") //设置模型apiKey
         .modelName("gpt-4o-mini") //设置模型名
         .build();
-//向模型提问
+        //向模型提问
         String answer = model.chat("你好");
-//输出结果
+        //输出结果
         System.out.println(answer);
     }
-
-
+    @Test
+    public void testOllama() {
+        //向模型提问
+        String answer = ollamaChatModel.chat("你好");
+        //输出结果
+        System.out.println(answer);
+    }
 }
