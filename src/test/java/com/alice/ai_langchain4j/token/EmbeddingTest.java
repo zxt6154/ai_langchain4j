622c0bd12eb29b517a9a55f02a7e6d03855cf678
zxt6154 <zxt61542990@yeah.net>
2025-12-24 17:38:19 +0800

    add

--- src/test/java/com/alice/ai_langchain4j/token/EmbeddingTest.java
+++ src/test/java/com/alice/ai_langchain4j/token/EmbeddingTest.java
@@ -1,8 +1,10 @@
 package com.alice.ai_langchain4j.token;
 
 import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.output.Response;
+import dev.langchain4j.store.embedding.EmbeddingStore;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,5 +20,30 @@ public class EmbeddingTest {
         System.out.println("向量维度：" + embed.content().vector().length);
         System.out.println("向量输出：" + embed.toString());
     }
+    @Autowired
+    private EmbeddingStore embeddingStore;
 
+    /**
+     * 将文本转换成向量，然后存储到pinecone中
+     *
+     * 参考：
+     * https://docs.langchain4j.dev/tutorials/embedding-stores
+     */
+    @Test
+    public void testPineconeEmbedded() {
+
+        //将文本转换成向量
+        TextSegment segment1 = TextSegment.from("我喜欢羽毛球");
+        Embedding embedding1 = embeddingModel.embed(segment1).content();
+        //存入向量数据库
+        embeddingStore.add(embedding1, segment1);
+
+        TextSegment segment2 = TextSegment.from("今天天气很好");
+        Embedding embedding2 = embeddingModel.embed(segment2).content();
+        embeddingStore.add(embedding2, segment2);
+    }
+    @Test
+    public void embeddingSearch() {
+
+    }
 }
